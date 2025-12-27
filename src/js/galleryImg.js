05933f0dd0b8a404f633e08037b7240b197244f3
Saydva <kacurikjozef@gmail.com>
2025-12-27 23:19:17 +0100

    repared sm screens

--- src/js/galleryImg.js
+++ src/js/galleryImg.js
@@ -23,13 +23,13 @@ function displayGallery() {
   galleryImages.forEach((image) => {
     const galleryItem = document.createElement('div');
     galleryItem.className =
-      'p-1.5 bg-white rounded-md flex flex-col items-center justify-center text-center hover:scale-110 hover:transition-transform hover:duration-500 hover:shadow-lg ';
+      'p-1.5 bg-white rounded-md flex flex-col items-center justify-center text-center hover:scale-110 hover:transition-transform hover:duration-500 hover:shadow-lg sm:sm';
 
     const img = document.createElement('img');
     img.src = image.src;
     img.alt = image.alt;
     img.onclick = () => OpenModal(image.src);
-    img.className = 'rounded-md object-contain cursor-pointer';
+    img.className = 'rounded-md object-contain cursor-pointer ';
 
     const caption = document.createElement('div');
     caption.className = 'mt-1.5 text-sm text-gray-800';
